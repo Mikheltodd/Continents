1beb01c297e2dafc2f36273f593998ff4318e6d9
Mikheltodd <68166999+Mikheltodd@users.noreply.github.com>
2025-03-05 19:20:33 -0500

    Continents and Main classes

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int continent = 5;
+        Continents myContinent = new Continents(continent);
+        System.out.println(myContinent);
+    }
+}
